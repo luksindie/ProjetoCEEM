c92c3d8c610fcd958ecfc9a2a8d8dc7b1df460a3
Rodrigo Gurgel <rodrigohenrique.gurgel@outlook.com>
2018-03-21 01:30:01 -0300

    Modelando o projeto

---
+++ ProjetoCEEM/Controllers/AdministrativaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoCEEM.Controllers
+{
+    public class AdministrativaController : Controller
+    {
+        // GET: Administrativa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
